4049d7a8b907abba5beadf9d6c3a94a0929a378b
JinMingTao <admin@JingMingTaos-Mac.local>
2014-05-26 20:37:10 +0900

    Initial Commit

---
+++ Bin001Tests/Bin001Tests.m
@@ -0,0 +1,34 @@
+//
+//  Bin001Tests.m
+//  Bin001Tests
+//
+//  Created by JinMingTao on 5/26/14.
+//  Copyright (c) 2014 jinmingtao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Bin001Tests : XCTestCase
+
+@end
+
+@implementation Bin001Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
